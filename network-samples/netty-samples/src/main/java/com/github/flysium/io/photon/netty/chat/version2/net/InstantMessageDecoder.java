9cb02e2b9ac4288df3844365a29b3d8bca4d9090
Sven Augustus <svenaugustus@outlook.com>
2020-06-02 20:34:09 +0800

    update netty samples for chat server and clients. (fix TCP stick package and unpacking)
    Signed-off-by: Sven Augustus <svenaugustus@outlook.com>

--- network-samples/netty-samples/src/main/java/com/github/flysium/io/photon/netty/chat/version2/net/InstantMessageDecoder.java
+++ network-samples/netty-samples/src/main/java/com/github/flysium/io/photon/netty/chat/version2/net/InstantMessageDecoder.java
@@ -19,8 +19,7 @@ package com.github.flysium.io.photon.netty.chat.version2.net;
 import com.github.flysium.io.photon.netty.chat.version2.model.InstantMessage;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.ByteToMessageDecoder;
-import java.util.List;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 
 /**
  * Decoder for <code>InstantMessage</code>
@@ -28,24 +27,50 @@ import java.util.List;
  * @author Sven Augustus
  * @version 1.0
  */
-public class InstantMessageDecoder extends ByteToMessageDecoder {
+// public class InstantMessageDecoder extends ByteToMessageDecoder {
+public class InstantMessageDecoder extends LengthFieldBasedFrameDecoder {
 
   // length ---- 32-bit integer
   public static final int BASE_LENGTH = 4;
 
-  @Override
-  protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
-    if (in.readableBytes() <= BASE_LENGTH) {
-      return;
-    }
-    // FIXME TCP stick package and unpacking
-    int length = in.readInt();
-    byte[] body = new byte[0];
-    if (length > 0) {
-      body = new byte[length];
-      in.readBytes(body);
-    }
-    out.add(InstantMessage.fromBytes(body));
+//  @Override
+//  protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+//    if (in.readableBytes() <= BASE_LENGTH) {
+//      return;
+//    }
+//    int length = in.readInt();
+//    byte[] body = new byte[0];
+//    if (length > 0) {
+//      body = new byte[length];
+//      in.readBytes(body);
+//    }
+//    out.add(InstantMessage.fromBytes(body));
+//  }
+
+  public InstantMessageDecoder() {
+    super(Integer.MAX_VALUE, 0, BASE_LENGTH);
   }
 
+  @Override
+  protected Object decode(ChannelHandlerContext ctx, ByteBuf buf) throws Exception {
+    ByteBuf in = (ByteBuf) super.decode(ctx, buf);
+
+    if (in == null) {
+      return null;
+    }
+    if (in.readableBytes() <= BASE_LENGTH) {
+      return null;
+    }
+    int length = in.readInt();
+    in.markReaderIndex();
+
+    if (length <= BASE_LENGTH) {
+      in.resetReaderIndex();
+      return null;
+    }
+    byte[] body = new byte[length];
+    in.readBytes(body);
+
+    return InstantMessage.fromBytes(body);
+  }
 }
