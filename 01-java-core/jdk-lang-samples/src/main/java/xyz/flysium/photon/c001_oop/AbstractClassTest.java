4fb91fbbe5ac262e3b0079700a5c8e62c4c14f2f
zeno (Sven Augustus) <zeno531@outlook.com>
2020-09-10 15:41:16 +0800

    samples for java keyword abstract
    Signed-off-by: zeno (Sven Augustus) <zeno531@outlook.com>

---
+++ 01-java-core/jdk-lang-samples/src/main/java/xyz/flysium/photon/c001_oop/AbstractClassTest.java
@@ -0,0 +1,19 @@
+package xyz.flysium.photon.c001_oop;
+
+/**
+ *
+ *
+ * @author zeno (Sven Augustus)
+ * @version 1.0
+ */
+public class AbstractClassTest {
+
+    static abstract class AbsC {
+
+        public abstract void m();
+
+        //        public abstract native void m();
+        //        public abstract synchronized void m();
+
+    }
+}
